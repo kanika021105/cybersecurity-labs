eb208402fedbbaf7a314a6110386f51c538c5777
kanika021105 <kanikasingh89625@gmail.com>
2026-05-28 23:50:39 +0530

    Create detector.py

---
+++ python-tools/login-detector/detector.py
@@ -0,0 +1,33 @@
+
+
+failed_attempts = {}
+
+log_file = open("login-log.txt", "r")
+
+lines = log_file.readlines()
+
+print("\n--- Suspicious Login Activity ---\n")
+
+for line in lines:
+
+    parts = line.strip().split()
+
+    ip = parts[0]
+    status = parts[1]
+
+    if status == "FAILED":
+
+        if ip not in failed_attempts:
+            failed_attempts[ip] = 1
+        else:
+            failed_attempts[ip] += 1
+
+
+for ip, count in failed_attempts.items():
+
+    print(f"{ip} → Failed Attempts: {count}")
+
+    if count >= 3:
+        print(f"⚠️ ALERT: Possible brute-force attack from {ip}\n")
+
+log_file.close()
